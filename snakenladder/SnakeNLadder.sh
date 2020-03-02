a10d4294bae27e465f9a1115454f4574fb2095e9
malivaibhv15 <pranali.lembhe77@gmail.com>
2020-03-02 12:09:55 +0530

    ADDED : Display dice position and dice count every time

--- snakenladder/SnakeNLadder.sh
+++ snakenladder/SnakeNLadder.sh
@@ -5,12 +5,14 @@ ladder=1
 snake=2
 option=0
 position=0
+count=0
 winningPosition=100
 function play()
 {
 	while [[ $position -lt $winningPosition ]]
 	do
 		dice=$(((RANDOM%6)+1))
+		((count++))
 		option=$((RANDOM%3))
 		case $option in
 			$number)
@@ -23,14 +25,16 @@ function play()
 				position=$(($position-$dice))
 				;;
 		esac
+		echo "The position of dice after dice role=$position"
 		if [[ $position -lt 0 ]]
 		then
 			position=0
 		fi
 		if [[ $position -gt 100 ]]
 		then
-			position=$(( $position-$dice ))
+			position=$(($position-$dice))
 		fi
 	done
+	echo "Number of times dice rolled=$count"
 }
 play
