f25adb6e7af7ca110d38a29c186f848097a36288
malivaibhv15 <pranali.lembhe77@gmail.com>
2020-03-02 10:45:00 +0530

    ADDED : Player reaches to 100 for first time

--- snakenladder/SnakeNLadder.sh
+++ snakenladder/SnakeNLadder.sh
@@ -5,20 +5,28 @@ ladder=1
 snake=2
 option=0
 position=0
+winningPosition=100
 function play()
 {
-	dice=$((RANDOM%6))
-	option=$((RANDOM%3))
-	case $option in
-		$number)
-			position=$(($position+$number))
-			;;
-		$ladder)
-			position=$(($position+$dice))
-			;;
-		$snake)
-			position=$(($position-$dice))
-			;;
-	esac
+	while [[ $position -lt $winningPosition ]]
+	do
+		dice=$((RANDOM%6))
+		option=$((RANDOM%3))
+		case $option in
+			$number)
+				position=$(($position+$number))
+				;;
+			$ladder)
+				position=$(($position+$dice))
+				;;
+			$snake)
+				position=$(($position-$dice))
+				;;
+		esac
+		if [[ $position -lt 0 ]]
+		then
+			position=0
+		fi
+	done
 }
 play
